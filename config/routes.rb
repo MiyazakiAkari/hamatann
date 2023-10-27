b45df0a713f7d790ae3bb63ec220bd923b95ec8b
MiyazakiAkari <mimrikaakizayami@gmail.com>
2023-10-28 07:47:01 +0900

    reviewのnew

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
     root 'lectures#home'
     resources :lectures do
       member do
-        get :review
+        get :reviews
+        get 'reviews/new', to: 'reviews#new' # 新規レビューフォームの表示
+        post 'reviews/create', to: 'reviews#create' # レビューレコードの作成
+        get 'reviews/edit', to: 'reviews#edit' # レビュー編集フォームの表示
+        patch 'reviews/update', to: 'reviews#update' # レビューレコードの更新
+        delete 'reviews/destroy', to: 'reviews#destroy' # レビューレコードの削除
       end
     end
 
