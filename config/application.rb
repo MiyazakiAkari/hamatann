654869283f2e34b58b123fd280fb06c592d9f7e2
MiyazakiAkari <mimrikaakizayami@gmail.com>
2023-11-01 20:15:28 +0900

    デプロイ用

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Hamatann
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << 'https://api.render.com/deploy/srv-cl12ohf5b13s73bgibjg?key=ZS_9VldOvg4'
   end
 end
